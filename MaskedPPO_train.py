71b7342283b700b8e46df7ab7593b63c058b8b9c
mj <mikulej7@gmail.com>
2024-03-23 20:29:48 +0100

    add plot_trend_line.py, plots add, custom networks

--- MaskedPPO_train.py
+++ MaskedPPO_train.py
@@ -2,19 +2,38 @@ from enviornement import MineSweeper
 from train import evaluate_mask
 
 from sb3_contrib import MaskablePPO
-from sb3_contrib.common.maskable.evaluation import evaluate_policy
-from stable_baselines3.common.env_checker import check_env
-#from sb3_contrib.common.maskable.utils import get_action_masks
-from sb3_contrib.common.maskable.callbacks import MaskableEvalCallback
-from stable_baselines3.common.monitor import Monitor
+# from sb3_contrib.common.maskable.evaluation import evaluate_policy
+# from stable_baselines3.common.env_checker import check_env
+# from sb3_contrib.common.maskable.utils import get_action_masks
+# from sb3_contrib.common.maskable.callbacks import MaskableEvalCallback
+# from stable_baselines3.common.monitor import Monitor
+
+import time
 import torch as th
+import pickle
+import matplotlib.pyplot as plt
+
 #Train Model
 env = MineSweeper(render_mode=None,sizeX=16,sizeY=16,bombs=40)
 if env == None:
     print("Error: Failed to create an enviornment")
-#check_env(env)
+    
+# https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
+# Custom actor (pi) and value function (vf) networks with Relu activation function
+# Note: an extra linear layer will be added on top of the pi and the vf nets, respectively
+policy_kwargs = dict(activation_fn=th.nn.ReLU,
+                     net_arch=[256, 64, 32])
+# policy_kwargs = dict(activation_fn=th.nn.ReLU,
+#                     net_arch=[256, 32])
+# policy_kwargs = dict(activation_fn=th.nn.ReLU,
+#                      net_arch=dict(pi=[32, 32], vf=[32, 32]))
+# policy_kwargs = dict(activation_fn=th.nn.ReLU,
+#                     net_arch=dict(pi=[256, 64, 32], vf=[256, 64, 32]))
+# policy_kwargs = dict(activation_fn=th.nn.Conv2d(in_channels=1,out_channels=1,kernel_size=5)
+#                      )
+
 observation, info = env.reset()
-model = MaskablePPO("MlpPolicy", env)
+model = MaskablePPO("MlpPolicy", env, policy_kwargs=policy_kwargs)
 # model = MaskablePPO("MlpPolicy", env,
 #                     learning_rate=0.0003,
 #                     n_steps= 2048,
@@ -22,26 +41,47 @@ model = MaskablePPO("MlpPolicy", env)
 #                     n_epochs= 10,
 #                     gamma= 0.4,policy_kwargs=dict(activation_fn=th.nn.ReLU, net_arch=[256, 256]))
 #,tensorboard_log="logs"
+
+performence_arr = []
+iterations_arr = []
+plt.subplots(figsize=(10, 5))
+
 print("Learning...")
-# steps = 1000
-# for i in range(1,10):
-#     model.learn(total_timesteps=steps,use_masking=True,reset_num_timesteps=False,tb_log_name="MaskPPO")
-#     model.save(f"models/{steps*i}")
-model.learn(total_timesteps=10,use_masking=True)
+steps = 10000
+for i in range(1,50):
+    start_time = time.time()
+    print("step=",i)
+    model.learn(total_timesteps=steps,use_masking=True,reset_num_timesteps=False,tb_log_name="MaskPPO")
+    model.save(f"models/{steps*i}")
+    print("Evaluating policy...")
+    performence = evaluate_mask(model,env,timesteps=500)
+    #performence = evaluate_random(model,env,timesteps=500)
+    stop_time = time.time()
+    print("Mean score: ",performence[0]," Win-rate: ",performence[1],"%")
+    print("--- %s seconds ---" % (stop_time - start_time))
+
+    performence_arr.append(performence[0])
+    iterations_arr.append(steps*i)
+    with open('total_performence.pkl', 'wb') as handle:
+        pickle.dump(performence_arr, handle)
+    with open('iterations_arr.pkl', 'wb') as handle:
+        pickle.dump(iterations_arr, handle)
+
+    plt.plot(iterations_arr,performence_arr)
+    plt.xlabel('Iteration')
+    plt.ylabel('Mean score')
+    plt.savefig('score_plot.png')
+
 print("Learning finished.")
+
 #Test Model
-print("Evaluating policy...")
-performence = evaluate_mask(model,env,timesteps=500)
-print("Mean score: ",performence[0]," Win-rate: ",performence[1],"%")
 print("Playing...")
 env = MineSweeper(render_mode="human",sizeX=16,sizeY=16,bombs=40)
 performence = evaluate_mask(model,env,timesteps=500)
-# performence = evaluate_policy(model,env,use_masking=True)
-# print("Mean reward=",performence[0]," Mean numbers of steps=",performence[1])
-
-
+print("Mean score: ",performence[0]," Win-rate: ",performence[1],"%")
 
 env.close()          
    
 #https://sb3-contrib.readthedocs.io/en/master/modules/ppo_mask.html
 #https://github.com/Stable-Baselines-Team/stable-baselines3-contrib/blob/master/sb3_contrib/common/envs/invalid_actions_env.py
+#https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
---
+++ plot_trend_line.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+dir_name='plots/2024-03-23_net_arch_256_64_32'
+with open(dir_name +'/total_performence.pkl', 'rb') as file:
+    total_performence = pickle.load(file)
+
+with open(dir_name +'/iterations_arr.pkl', 'rb') as file:
+    iterations_arr = pickle.load(file)
+
+x = iterations_arr
+y = total_performence
+
+#create scatterplot
+plt.subplots(figsize=(10, 5))
+plt.scatter(x, y, color='blue')
+
+#calculate equation for quadratic trendline
+z = np.polyfit(x, y, 4)
+p = np.poly1d(z)
+
+#add trendline to plot
+plt.plot(x, p(x), color='orange')
+
+plt.xlabel('Iteration')
+plt.ylabel('Mean score')
+plt.savefig(dir_name +'/score_trend.png')
