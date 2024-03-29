e5146d2434e99771b92cc4cca634f7c4eb7f8d24
mj <mikulej7@gmail.com>
2024-03-29 13:08:23 +0100

    Convolution layer fixed

--- custom_policy.py
+++ custom_policy.py
@@ -55,25 +55,6 @@ import torch.nn.functional as F
 #         return self.linear(self.cnn(observations))
 
 
-class MyNetwork(BaseFeaturesExtractor):
-    """
-    :param observation_space: (gym.Space)
-    :param features_dim: (int) Number of features extracted.
-        This corresponds to the number of unit for the last layer.
-    """
-
-    def __init__(self, observation_space: spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim: int = 256):
-        super().__init__(spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim)
-        self.l1 = nn.Linear(features_dim,16)
-        self.l2 = nn.Linear(16,8)
-        self.l3 = nn.Linear(8,features_dim)
-
-    def forward(self, observations: th.Tensor) -> th.Tensor:
-        observations = th.flatten(observations,start_dim=1)
-        observations = F.relu(self.l1(observations))
-        observations = F.relu(self.l2(observations))
-        observations = F.relu(self.l3(observations))
-        return observations
 # class MyNetwork(BaseFeaturesExtractor):
 #     """
 #     :param observation_space: (gym.Space)
@@ -83,13 +64,34 @@ class MyNetwork(BaseFeaturesExtractor):
 
 #     def __init__(self, observation_space: spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim: int = 256):
 #         super().__init__(spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim)
-#         self.l1 = nn.Conv1d(features_dim, 32, kernel_size=5, stride=1, padding=0)
-#         self.l2 = nn.Linear(32,features_dim)
+#         self.l1 = nn.Linear(features_dim,16)
+#         self.l2 = nn.Linear(16,8)
+#         self.l3 = nn.Linear(8,features_dim)
 
 #     def forward(self, observations: th.Tensor) -> th.Tensor:
 #         observations = th.flatten(observations,start_dim=1)
 #         observations = F.relu(self.l1(observations))
 #         observations = F.relu(self.l2(observations))
+#         observations = F.relu(self.l3(observations))
 #         return observations
+class MyNetwork(BaseFeaturesExtractor):
+    """
+    :param observation_space: (gym.Space)
+    :param features_dim: (int) Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    """
+
+    def __init__(self, observation_space: spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim: int = 256):
+        super().__init__(spaces.Box(low=-1,high=9,shape=(16, 16),dtype=np.int8), features_dim)
+        self.l1 = nn.Conv2d(1, 1, kernel_size=(5,5), stride=1, padding=2,padding_mode='zeros')
+        self.l2 = nn.Linear(features_dim,features_dim)
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        #observations = th.flatten(observations,start_dim=1)
+        observations = th.unsqueeze(observations,1)
+        observations = F.relu(self.l1(observations))
+        observations = th.flatten(observations,start_dim=1)
+        observations = F.relu(self.l2(observations))
+        return observations
 
 #https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html#default-network-architecture
