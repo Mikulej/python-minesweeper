0b1e8f1dbf63b2e271f5b4302e609d94a86a9b21
mj <mikulej7@gmail.com>
2024-05-08 23:48:50 +0200

    evaluate fix, measure 20 games

--- train.py
+++ train.py
@@ -1,15 +1,14 @@
-import time
+
 def evaluate_mask(model, env, timesteps):
     scores = 0
     numOfGames = 0
     numOfWins = 0
     observation, info = env.reset()
-    for _ in range(timesteps):       
+    while(numOfGames < 20):      
         invalidActions = env.action_masks()
         action, states = model.predict(observation,action_masks=invalidActions,deterministic=False)
         observation, reward, terminated,truncated, info = env.step(action)
         env.render()
-        #time.sleep(1)
         if terminated:
             scores += env.score
             numOfGames += 1
@@ -24,11 +23,10 @@ def evaluate(model, env, timesteps):
     numOfGames = 0
     numOfWins = 0
     observation, info = env.reset()
-    for _ in range(timesteps): 
+    while(numOfGames < 20):
         action, states = model.predict(observation,deterministic=False)
         observation, reward, terminated,truncated, info = env.step(action)
         env.render()
-        #time.sleep(1)
         if terminated:
             scores += env.score
             numOfGames += 1
